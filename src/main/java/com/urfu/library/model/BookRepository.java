752dc0982b6b14e40391b440e31926099e326197
Alexandr Filatov <filatovalexandr2003@gmail.com>
2024-11-07 14:08:51 +0500

    fix: entity, service methods & some tests

--- src/main/java/com/urfu/library/model/BookRepository.java
+++ src/main/java/com/urfu/library/model/BookRepository.java
@@ -5,7 +5,13 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+/**
+ * Репозиторий сущности Book для взаимодействия с базой данных
+ */
 @Repository
-public interface BookRepository extends JpaRepository<Book, Integer> {
+public interface BookRepository extends JpaRepository<Book, Long> {
+    /**
+     * Поиск книг по заданному автору
+     */
     List<Book> findByTitle(String author);
 }
